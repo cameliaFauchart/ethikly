fecea6d8a8b47571efcea727f5c16f6ae084e11d
Flavie Parmentier <flavie.parmentier@gmail.com>
2021-12-01 17:20:54 +0100

    wip

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,12 +1,22 @@
 class WishlistsController < ApplicationController
 
   def create
-    @wishlist = Whishlist.new
     @wishlist = Offer.find(params[:offer_id])
+    @wishlist = Wishlist.new(wishlist_params)
+    @wishlist.offer = @offer
+    @wishlist.user = current_user
+    if @wishlist.save
+
   end
 
   def destroy
 
+  end
+
+  private
+
+  def wishlist_params
+    params.require(:wishlist).permit(:destroy)
   end
 
 end
