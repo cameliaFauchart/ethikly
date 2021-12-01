a67f52f49eb897db4a9ed09da059b489c4740ff1
Camelia Fauchart <cami.chani1@gmail.com>
2021-12-01 12:14:48 +0100

    resoureces et banner homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :offers, only: [:index, :show] do
-    resources :whishlists only: [:create]
+    resources :whishlists, only: [:create]
   end
   resources :wishlists, only: [:destroy]
 end
