5e07c6435d145636538674d0ebe6b29c0146618b
Camelia Fauchart <cami.chani1@gmail.com>
2021-12-02 14:39:04 +0100

    fonctionnement de la barre de recherche

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,13 @@
 class OffersController < ApplicationController
   def index
+   
+    if params[:ou].present? || params[:quoi].present?
+      sql_query = "offers.name ILIKE :quoi AND companies.city ILIKE :ou"
+      @offers = Offer.joins(:company).where(sql_query, ou: "%#{params[:ou]}%",quoi: "%#{params[:quoi]}%")
+       
+    else
     @offers = Offer.all
+    end
   end
 
   def show
