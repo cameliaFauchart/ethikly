13329982540090007ddcafe7e4c7446c5fce2c4d
selom azakpo <selom.ya@outlook.fr>
2021-12-02 18:03:54 +0100

    still working on the filter

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,14 @@
 class OffersController < ApplicationController
   def index
-    @offers = Offer.includes(:company)
+
+    if params[:search].present? && !(params[:search].each_value.all? &:empty?)
+
+      @offers = Offer.where("income > :value", value: params[:search][:income])
+
+
+    else
+      @offers = Offer.includes(:company)
+    end
   end
 
   def show
@@ -8,3 +16,5 @@ class OffersController < ApplicationController
     @wishlist = Wishlist.new
   end
 end
+# Book.where("created_at >= :start_date AND created_at <= :end_date",
+#   {start_date: params[:start_date], end_date: params[:end_date]})
