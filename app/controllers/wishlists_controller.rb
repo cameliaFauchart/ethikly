c9f3e9eabeecc2e0829162411135ef1eab216f86
Flavie Parmentier <flavie.parmentier@gmail.com>
2021-12-02 16:59:15 +0100

    coeurs favoris

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -2,11 +2,11 @@ class WishlistsController < ApplicationController
 
   def create
     @offer = Offer.find(params[:offer_id])
-    @wishlist = Wishlist.new()
+    @wishlist = Wishlist.new() # ()?
     @wishlist.offer = @offer
     @wishlist.user = current_user
-    if @wishlist.save!
-      redirect_to wishlists_path
+    if @wishlist.save
+      redirect_to request.referer
     end
 
   end
@@ -14,7 +14,9 @@ class WishlistsController < ApplicationController
   def destroy
     @wishlist = Wishlist.find(params[:id])
     @wishlist.destroy
+    redirect_to request.referer # redirige-moi là ou j'étais précédemment
   end
+
   def index
     @wishlists = Wishlist.all
   end
