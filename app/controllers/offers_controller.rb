66b3d0be7d77947a08ca07f8abc787581677e2c0
selom azakpo <selom.ya@outlook.fr>
2021-12-03 12:55:25 +0100

    filter functionnal

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,23 +2,20 @@ class OffersController < ApplicationController
   def index
 
     if params[:search].present? && !(params[:search].each_value.all? &:empty?)
-
       @offers = []
       @selected_offers = Offer.where("income > :value", value: params[:search][:income])
-
       @selected_offers.each do |offer|
         company = Company.find(offer.company_id)
-
-
         if company.city == params[:search][:city]
           @offers << offer
         end
       end
 
+      if @offers.size == 0
+        @offers = @selected_offers
+      end
     else
-
       @offers = Offer.all
-
     end
   end
 
@@ -27,5 +24,3 @@ class OffersController < ApplicationController
     @wishlist = Wishlist.new
   end
 end
-# Book.where("created_at >= :start_date AND created_at <= :end_date",
-#   {start_date: params[:start_date], end_date: params[:end_date]})
