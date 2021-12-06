ea7be10c8f1ff41ed54de477f178021c04661208
Flavie Parmentier <flavie.parmentier@gmail.com>
2021-12-06 23:27:39 +0100

    wip

--- app/javascript/channels/*_animated_text_test.js
+++ app/javascript/channels/*_animated_text_test.js
@@ -1,7 +1,24 @@
-const text = document.getElementById("js-test");
-const splitText = text.innerText.split("");
+const text = document.getElementById("js-text");
+const splitText = text.innerText.split(""); // séparer chaque caractère
+
+text.innerHTML = "";
+let sum = 0;
+// setInterval répète la méthode avec un délai fixé
+// entre chaque appel
+setInterval(AddLetter()[100]);
+
+const AddLetter() => {
+  if (sum < splitText.length) {
+    text.innerHTML += splitText[sum];
+    sum++;
+  }
+}
+
+// Code test pas encore efficient
 
 
 
 
-export { };
+
+
+export { AddLetter };
