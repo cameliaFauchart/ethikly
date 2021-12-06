3397124275da22d0374e979f0f0fef3db6086d14
Camelia Fauchart <cami.chani1@gmail.com>
2021-12-06 14:26:14 +0100

    modif filter et search

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,29 +2,30 @@ class OffersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
   def index
+    str=""
+    p params
 
+    # if params[:search].present? && !(params[:search].each_value.all? &:empty?)
+    #   @offers = []
+    #   @selected_offers = Offer.where("income > :value", value: params[:search][:income])
+    #   @selected_offers.each do |offer|
+    #     job = Offer.find(offer.id).contract
+    #     if job == params[:search][:contract]
+    #       @offers << offer
+    #     end
+    #   end
 
-    if params[:search].present? && !(params[:search].each_value.all? &:empty?)
-      @offers = []
-      @selected_offers = Offer.where("income > :value", value: params[:search][:income])
-      @selected_offers.each do |offer|
-        job = Offer.find(offer.id).contract
-        if job == params[:search][:contract]
-          @offers << offer
-        end
-      end
-
-      if @offers.size == 0
-        @offers = @selected_offers
-      end
+    #   if @offers.size == 0
+    #     @offers = @selected_offers
+    #   end
 
     #if params[:ou].present? || params[:quoi].present?
       #sql_query = "offers.name ILIKE :quoi AND companies.city ILIKE :ou"
       #@offers = Offer.joins(:company).where(sql_query, ou: "%#{params[:ou]}%",quoi: "%#{params[:quoi]}%")
 
-    else
+   
       @offers = Offer.all
-    end
+   
   end
 
   def show
