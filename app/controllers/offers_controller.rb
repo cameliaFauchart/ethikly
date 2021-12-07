afd8adb72e04ee0b0f6a253666c99f1b6e0df1fb
Camelia Fauchart <cami.chani1@gmail.com>
2021-12-07 09:07:40 +0100

    filters and search bar

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -25,30 +25,7 @@ class OffersController < ApplicationController
     else
       @offers = Offer.all
     end
-  end
- 
-    
-   #
-    # if params[:search].present? && !(params[:search].each_value.all? &:empty?)
-    #   @offers = []
-    #   @selected_offers = Offer.where("income > :value", value: params[:search][:income])
-    #   @selected_offers.each do |offer|
-    #     job = Offer.find(offer.id).contract
-    #     if job == params[:search][:contract]
-    #       @offers << offer
-    #     end
-    #   end
-
-    #   if @offers.size == 0
-    #     @offers = @selected_offers
-    #   end
-
-    #if params[:ou].present? || params[:quoi].present?
-      #sql_query = "offers.name ILIKE :quoi AND companies.city ILIKE :ou"
-      #@offers = Offer.joins(:company).where(sql_query, ou: "%#{params[:ou]}%",quoi: "%#{params[:quoi]}%")
-
-   
-     
+  end  
 
   def show
     @offer = Offer.find(params[:id])
