ebfc674f51e4b77d3271383fb30e655e1b89afd5
Flavie Parmentier <flavie.parmentier@gmail.com>
2021-12-08 14:07:20 +0100

    config suite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+
+  def default_url_options
+  { host: ENV["ethikly"] || "localhost:3000" }
+  end
+
   def change_avatar
     current_user.update(avatar_params)
     redirect_to wishlists_path
